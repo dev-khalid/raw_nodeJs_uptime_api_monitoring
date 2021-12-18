9bae0520ef15183a539f122fbebcba204150f276
dev-khalid <khalid.hossain.akash@gmail.com>
2021-12-18 07:04:26 +0600

    part 4 authentication

--- handlers/routeHandlers/userHandler.js
+++ handlers/routeHandlers/userHandler.js
@@ -1,5 +1,6 @@
 const { hash, parseJSON } = require('../../helpers/utilities');
 const data = require('../../lib/data');
+const tokenHandler = require('./tokenHandler');
 
 const handler = {};
 
@@ -20,18 +21,33 @@ handler._user.get = (requestProperties, callback) => {
       ? requestProperties.queryStringObject.phone
       : false;
   if (phone) {
-    //user er data phone number er basis a read korar try korbo .
-    data.read('user', phone, (err, u) => {
-      if (!err) {
-        const user = { ...parseJSON(u) };
-        delete user.password;
-        callback(200, user);
+    // verify token
+    const token =
+      typeof requestProperties.headersObject.token === 'string'
+        ? requestProperties.headersObject.token
+        : false;
+
+    tokenHandler._token.verify(token, phone, (tokenId) => {
+      if (tokenId) {
+        // lookup the user
+        data.read('user', phone, (err, u) => {
+          if (!err) {
+            const user = { ...parseJSON(u) };
+            delete user.password;
+            callback(200, user);
+          } else {
+            callback(404, {
+              error: 'User Not Found!',
+            });
+          }
+        });
       } else {
-        callback(404, {
-          error: 'User Not Found!',
+        callback(403, {
+          error: 'Authentication failure!',
         });
       }
     });
+    //user er data phone number er basis a read korar try korbo .
   } else {
     callback(400, {
       error: 'There is a problem in your request!',
@@ -129,36 +145,49 @@ handler._user.put = (requestProperties, callback) => {
 
   if (phone) {
     if (firstName || lastName || password) {
-      //ebar age phone number ta check dite hobe .
-      data.read('user', phone, (err1, uData) => {
-        if (!err1 && uData) {
-          //ebar ei udata jehetu file system theke asche so eitake json.parse kore nite hobe .
-          const userData = { ...parseJSON(uData) };
-          //ebar amake update kore dite hobe .
-          if (firstName) {
-            userData.firstName = firstName;
-          }
-          if (lastName) {
-            userData.lastName = lastName;
-          }
-          if (password) {
-            userData.password = hash(password);
-          }
+      const token =
+        typeof requestProperties.headersObject.token === 'string'
+          ? requestProperties.headersObject.token
+          : false;
 
-          data.update('user', phone, userData, (err2) => {
-            if (!err2) {
-              callback(200, {
-                message: 'user updated Successfully!',
+      tokenHandler._token.verify(token, phone, (tokenId) => {
+        if (tokenId) {
+          // loopkup the user
+          data.read('users', phone, (err1, uData) => {
+            const userData = { ...parseJSON(uData) };
+
+            if (!err1 && userData) {
+              if (firstName) {
+                userData.firstName = firstName;
+              }
+              if (lastName) {
+                userData.firstName = firstName;
+              }
+              if (password) {
+                userData.password = hash(password);
+              }
+
+              // store to database
+              data.update('users', phone, userData, (err2) => {
+                if (!err2) {
+                  callback(200, {
+                    message: 'User was updated successfully!',
+                  });
+                } else {
+                  callback(500, {
+                    error: 'There was a problem in the server side!',
+                  });
+                }
               });
             } else {
-              callback(500, {
-                error: 'there was an error in server side!',
+              callback(400, {
+                error: 'You have a problem in your request!',
               });
             }
           });
         } else {
-          callback(400, {
-            error: 'Phone number does not match!',
+          callback(403, {
+            error: 'Authentication failure!',
           });
         }
       });
@@ -174,36 +203,54 @@ handler._user.put = (requestProperties, callback) => {
   }
   //jodi file ta read kora jay tahole amra update korar jonne data.update method ke call kore update kore dibo . and amar mote ami ekhane kono prokare kono pure update korbo na ekdom full replace kore dibo .
 };
-handler._user.delete = (requestProperties, callback) => {
+handler._users.delete = (requestProperties, callback) => {
+  // check the phone number if valid
   const phone =
     typeof requestProperties.queryStringObject.phone === 'string' &&
     requestProperties.queryStringObject.phone.trim().length === 11
       ? requestProperties.queryStringObject.phone
       : false;
+
   if (phone) {
-    data.read('user', phone, (err, userData) => {
-      if (!err && userData) {
-        data.delete('user', phone, (err2) => {
-          if (!err2) {
-            callback(204, {
-              message: 'User Deleted Successfully!',
+    // verify token
+    const token =
+      typeof requestProperties.headersObject.token === 'string'
+        ? requestProperties.headersObject.token
+        : false;
+
+    tokenHandler._token.verify(token, phone, (tokenId) => {
+      if (tokenId) {
+        // lookup the user
+        data.read('users', phone, (err1, userData) => {
+          if (!err1 && userData) {
+            data.delete('users', phone, (err2) => {
+              if (!err2) {
+                callback(200, {
+                  message: 'User was successfully deleted!',
+                });
+              } else {
+                callback(500, {
+                  error: 'There was a server side error!',
+                });
+              }
             });
           } else {
             callback(500, {
-              error: 'Server side Error',
+              error: 'There was a server side error!',
             });
           }
         });
       } else {
-        callback(404, {
-          error: 'Phone number does not exist!',
+        callback(403, {
+          error: 'Authentication failure!',
         });
       }
     });
   } else {
     callback(400, {
-      error: 'Invalid Phone Number!',
+      error: 'There was a problem in your request!',
     });
   }
 };
+
 module.exports = handler;
--- helpers/utilities.js
+++ helpers/utilities.js
@@ -19,4 +19,21 @@ utilities.hash = (stringData) => {
   }
   return false;
 };
+utilities.createRandomString = (strlength) => {
+  let length = strlength;
+  length = typeof strlength === 'number' && strlength > 0 ? strlength : false;
+
+  if (length) {
+    const possiblecharacters = 'abcdefghijklmnopqrstuvwxyz1234567890';
+    let output = '';
+    for (let i = 1; i <= length; i += 1) {
+      const randomCharacter = possiblecharacters.charAt(
+        Math.floor(Math.random() * possiblecharacters.length)
+      );
+      output += randomCharacter;
+    }
+    return output;
+  }
+  return false;
+};
 module.exports = utilities;
